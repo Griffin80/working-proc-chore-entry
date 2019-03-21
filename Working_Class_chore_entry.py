3b4fd7602e7a04dda4eb9644eb5c7ee24828559d
Mike Doyle <thecodingstage@gmail.com>
2019-03-21 13:33:41 -0400

    adds labels for entry categories

--- Working_Class_chore_entry.py
+++ Working_Class_chore_entry.py
@@ -1,10 +1,6 @@
 from tkinter import *
 import json
 
-
-
-
-
 chore_file = 'Class chore file.json'
 
 try:
@@ -23,11 +19,11 @@ class ChoreEntry(Frame):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.name_label = Label(self,text='Name')
+        self.name_label = Label(self, text='Name')
         self.name_label.grid(row=0, column=0)
         self.chore_name = Entry(self)
         self.chore_name.grid(row=1, column=0)
-        self.dorm_label = Label(self,text='Dorm')
+        self.dorm_label = Label(self, text='Dorm')
         self.dorm_label.grid(row=0, column=1)
         self.chore_dorm = Entry(self)
         self.chore_dorm.grid(row=1, column=1)
@@ -41,7 +37,10 @@ class ChoreEntry(Frame):
         self.branch_entry.grid(row=1, column=3)
 
         self.button = Button(self, text='button', command=self.add)
-        self.button.grid(row=2,columnspan=4)
+        self.button.grid(row=2, columnspan=4)
+
+        self.indication_label = Label(self)
+        self.indication_label.grid(row=2, column=3)
 
     def add(self):
         new_dict = {self.chore_name.get(): {'dorm': self.chore_dorm.get(), 'handicap': self.chore_handicap.get()}}
@@ -53,13 +52,34 @@ class ChoreEntry(Frame):
         else:
 
             for part_of_day in data['Chores'][self.branch_entry.get()]:
-                data['Chores'][self.branch_entry.get()][part_of_day].append(new_dict)
+                if new_dict not in data['Chores'][self.branch_entry.get()][part_of_day]:
+                    data['Chores'][self.branch_entry.get()][part_of_day].append(new_dict)
+                    self.indication_label.configure(text='Entered', fg='green')
+                else:
+                    self.indication_label.configure(text='Already in the system', fg='red')
 
         data_rep = json.dumps(data, indent=2)
         print('chore added: ', data_rep)
 
 
+# class EntryArray(Frame):
+#
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#
+#         for category in data['Chores']:
+#             self.button = Button(self, text=category, command=self.create_array)
+#             self.button.grid()
+#
+#     def create_array(self):
+#
+#         print(self.button['text'])
+
+
 root = Tk()
+
+# array = EntryArray(root)
+# array.grid(row=0, column=0)
 
 chore_entry = ChoreEntry(root)
 chore_entry.grid(row=0)
